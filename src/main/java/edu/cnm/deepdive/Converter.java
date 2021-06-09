9cd6aff4c101eb18262b5d92127e30a5c2aa1883
Lee Scroggins <lscrggns@yahoo.com>
2021-06-08 19:13:26 -0600

    Input convertF2C, line 18 added if statement.

--- src/main/java/edu/cnm/deepdive/Converter.java
+++ src/main/java/edu/cnm/deepdive/Converter.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Converter {
 
-  private static final String INPUT_PROMPT = "Please type a Celsius temperature (blank to quit): ";
+  private static final String INPUT_PROMPT = "Please type a Celsius temperature. (blank to quit): ";
   private static final String FAHRENHEIT_OUTPUT_FORMAT =
       "Fahrenheit temperature: %,.2f degrees%nPlease type a Celsius temperature (blank to quit): ";
   private static final String PARSE_EXCEPTION_FORMAT = "Unable to parse \"%s\".  Please try again: ";
@@ -15,20 +15,25 @@ public class Converter {
     String userInput = scanner.nextLine().trim();
     while (!userInput.isEmpty()) {
       try {
+        if (userInput.toLowerCase().endsWith("f")) {
+          System.out.println(convertF2C(Double.parseDouble(userInput)));
+        }
+
         //TODO Check last character of userInput; if 'f', treat as Fahrenheit; otherwise, Celsius.
         System.out.printf(FAHRENHEIT_OUTPUT_FORMAT, convertC2F(Double.parseDouble(userInput)));
       } catch (NumberFormatException e) {
         System.out.printf(PARSE_EXCEPTION_FORMAT, userInput);
       }
+
       userInput = scanner.nextLine().trim();
     }
   }
 
-  public static double convertC2F(double celsius) {
-    return celsius * 9 / 5 + 32;
-  }
+  public static double convertC2F(double celsius) { return celsius * 9 / 5 + 32;  }
 
-  public static double convertF2C(double fahrenheit) {
-    return 0; //TODO Implement the Fahrenheit-to-Celsius conversion formula.
-  }
+  public static double convertF2C(double fahrenheit) { return (fahrenheit - 32) * 5 / 9; }
+
+    //TODO Implement the Fahrenheit-to-Celsius conversion formula.
+
 }
+
