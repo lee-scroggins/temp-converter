7f7f836aae407315c8cdbf2ec756a17e371074ed
Lee Scroggins <lscrggns@yahoo.com>
2021-06-09 11:55:41 -0600

    Started implementation of dual-scale conversion.

--- src/main/java/edu/cnm/deepdive/Converter.java
+++ src/main/java/edu/cnm/deepdive/Converter.java
@@ -1,12 +1,13 @@
 package edu.cnm.deepdive;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Converter {
 
-  private static final String INPUT_PROMPT = "Please type a Celsius temperature. (blank to quit): ";
+  private static final String INPUT_PROMPT = "Please type a Celsius or Fahrenheit (ending with 'f') temperature. (blank to quit): ";
   private static final String FAHRENHEIT_OUTPUT_FORMAT =
-      "Fahrenheit temperature: %,.2f degrees%nPlease type a Celsius temperature (blank to quit): ";
+      "Fahrenheit temperature: %,.2f degrees%nPlease type a Celsius or Fahrenheit (ending with 'f') temperature (blank to quit): ";
   private static final String PARSE_EXCEPTION_FORMAT = "Unable to parse \"%s\".  Please try again: ";
   private static final double FAHRENHEIT_PER_CELSIUS = 9.0 / 5;
   private static final int FAHRENHEIT_FREEZING_POINT = 32;
@@ -14,20 +15,20 @@ public class Converter {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.print(INPUT_PROMPT);
-    String userInput = scanner.nextLine().trim();
+    String userInput = scanner.nextLine().trim().toLowerCase();
     while (!userInput.isEmpty()) {
       try {
-        if (userInput.toLowerCase().endsWith("f")) {
-          System.out.println(convertF2C(Double.parseDouble(userInput)));
+        char lastChar = userInput.charAt(userInput.length() - 1);
+        if (lastChar == 'f') {
+          //TODO Convert everything in userInput BUT the last character from fahrenheit to celsius
+        } else {
+          System.out.printf(FAHRENHEIT_OUTPUT_FORMAT, convertC2F(Double.parseDouble(userInput)));
         }
-
-        //TODO Check last character of userInput; if 'f', treat as Fahrenheit; otherwise, Celsius.
-        System.out.printf(FAHRENHEIT_OUTPUT_FORMAT, convertC2F(Double.parseDouble(userInput)));
       } catch (NumberFormatException e) {
         System.out.printf(PARSE_EXCEPTION_FORMAT, userInput);
       }
 
-      userInput = scanner.nextLine().trim();
+      userInput = scanner.nextLine().trim().toLowerCase();
     }
   }
 
