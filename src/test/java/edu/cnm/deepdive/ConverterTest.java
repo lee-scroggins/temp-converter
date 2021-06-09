1b795b6b198d62b28feed83e24d08cf5014a3360
Lee Scroggins <lscrggns@yahoo.com>
2021-06-09 11:33:29 -0600

    Refactored unit tests.

--- src/test/java/edu/cnm/deepdive/ConverterTest.java
+++ src/test/java/edu/cnm/deepdive/ConverterTest.java
@@ -9,18 +9,32 @@ class ConverterTest {
   static double[] celsiusValues = {-40, 0, 100, 37.1};
   static double[] expectedFahrenheitValues = {-40, 32, 212, 98.78};
   static final double TOLERANCE = 0.0001;
+  static final double[][] temperaturePairs = {
+      {-40, -40},
+      {0, 32},
+      {100, 212},
+      {37.1, 98.78}
+  };
 
   @Test
   void convertC2F() {
-   for (int i = 0; i < celsiusValues.length; i++) {
-     double celsius = celsiusValues[i];
-     double expectedFahrenheit = expectedFahrenheitValues[i];
-     double actualFahrenheiht = Converter.convertC2F(celsius);
-     assertEquals(expectedFahrenheit, actualFahrenheiht, TOLERANCE);
-   }
+    for (double[] testCase : temperaturePairs) {
+      double celsius = testCase[0];
+      double expectedFahrenheit = testCase[1];
+      double actualFahrenheit = Converter.convertC2F(celsius);
+      assertEquals(expectedFahrenheit, actualFahrenheit, TOLERANCE);
+    }
   }
 
   @Test
   void convertF2C() {
-  } //TODO Implement test for Fahrenheit-to-Celsius conversion.
+    for (double[] testCase : temperaturePairs) {
+      double fahrenheit = testCase[1];
+      double expectedCelsius = testCase[0];
+      double actualCelsius = Converter.convertF2C(fahrenheit);
+      assertEquals(expectedCelsius, actualCelsius, TOLERANCE);
+    }
+
+  }
+
 }
